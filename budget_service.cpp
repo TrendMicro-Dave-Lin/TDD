994b21cdf7a649e47ca9a0e0b8373c7d86a146d9
Dave Lin (RD) <dave_lin@trendmicro.com>
2023-08-29 15:52:58 +0800

    202030829 init

---
+++ budget_service.cpp
@@ -0,0 +1,18 @@
+
+
+class budget_service
+{
+private:
+    /* data */
+public:
+    budget_service(date start, date end);
+    ~budget_service();
+};
+
+budget_service::budget_service(/* args */)
+{
+}
+
+budget_service::~budget_service()
+{
+}
